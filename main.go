5114a40da85f3239d1a3ba1e26a6ef683a2d41e4
Oleksandr Stepanenko <ostepa@softserveinc.com>
2017-11-27 09:52:10 +0200

    time

--- main.go
+++ main.go
@@ -1,34 +1,74 @@
 package main
 
-import "fmt"
 import (
+	"fmt"
 	"math/rand"
-	"math"
+	"time"
 )
 
-var(
-	str string = "HelloWorld"
-	n uint8 = 1
-	x int8 = 15
+var (
+	x uint8 = 255
 )
+
 
 func main() {
-	fmt.Println(len(str))
-	fmt.Println(str[n])
-	fmt.Println(n % 5)
 
-	var inp float64
-	fmt.Print("Enter F temp: ")
-	fmt.Scanf("%f", &inp)
-	var res float64
-	res = (inp - 32) * 5 / 9
-	fmt.Println("Celsium:", math.Ceil(res))
+	/*var input string
+	fmt.Printf("Hello user #%d!\n", rand.Int())
+	fmt.Scanf("%s", &input)
+	fmt.Println(input)*/
 
 
-	fmt.Println(rand.Intn(10))
-	fmt.Println(rand.Intn(10))
-	fmt.Println(rand.Intn(50))
-	fmt.Println(rand.Int())
+	/*for i:= 1; i <= 10; i++ {
+		if i % 2 == 0 {
+			fmt.Printf("%d even\n", i)
+		} else {
+			fmt.Printf("%d odd\n", i)
+		}
+	}*/
+
+	var seed int64 = time.Now().UnixNano()
+	rand.Seed(seed)
+
+	var wDay int = rand.Intn(7)
+
+	switch wDay {
+		case 0: fmt.Printf("Mon\n"); break
+		case 1: fmt.Printf("Tu\n"); break
+		case 2: fmt.Printf("Wh\n"); break
+		case 3: fmt.Printf("Th\n"); break
+		case 4: fmt.Printf("Fr\n"); break
+		case 5: fmt.Printf("St\n"); break
+		case 6: fmt.Printf("Sd\n"); break
+	}
+
+	loc, err := time.LoadLocation("Europe/Rome")
+
+	fmt.Printf("%d\n%d\n", time.Now().Unix(), time.Now().UnixNano())
+
+	tomorrow:= time.Date(2017, 11, 26, 0, 0, 0, 0, time.Local)
+	fmt.Println(time.Until(tomorrow))
+
+	//customTimeValue:= time.Unix( time.Now().Unix() + 4000, 0 )
+	//fmt.Println(time.Until(customTimeValue))
+
+	fmt.Println(time.Unix(1511641749, 808104745))
+
+
+	//dUTC:= time.Date(2017, 11, 25, 23, 30, 10, 0, time.UTC)
+	//dLOcal:= time.Date(2017, 11, 25, 23, 30, 10, 0, time.Local)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(time.Now().In(loc).String())
+
+	fmt.Println(tomorrow.Unix())
+
+
+	fmt.Println(time.Local)
+
 }
 
 
